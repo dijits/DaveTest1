97ff4a2138fd7fe1e263aa97062d62c04abd5c60
dijits <dijits@hotmail.com>
2013-09-26 13:08:40 +1000

    Update execAllSQL.sh

--- execAllSQL.sh
+++ execAllSQL.sh
@@ -4,5 +4,5 @@ username=$1
 password=$2
 for sqlfile in `find . -name '*.sql' -print`
 do
-    sqlplus $username/$password@CSUAT <  $sqlfile
+    /opt/oracle/product/11.2.0.3/client_1/bin/sqlplus $username/$password@CSUAT <  $sqlfile
 done
