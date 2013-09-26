6434c711ba812d9d6ed319df58d92e923d69dbf5
dijits <dijits@hotmail.com>
2013-09-26 14:48:53 +1000

    Update execAllSQL.sh

--- execAllSQL.sh
+++ execAllSQL.sh
@@ -6,4 +6,8 @@ for sqlfile in `find . -name '*.sql' -print`
 do
     echo $sqlfile $# $username $password
     /opt/oracle/product/11.2.0.3/client_1/bin/sqlplus "$username/$password@CSUAT" <  $sqlfile
+    if [ $? -ne 0 ]; then
+        echo "ERROR! SQL*Plus failed..."
+      exit 1
+    fi
 done
